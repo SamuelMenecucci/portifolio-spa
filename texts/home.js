e53469fee58b4abb3158d1794838639aabc8c45e
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-09-30 16:24:13 -0300

    feat: home page texts

---
+++ texts/home.js
@@ -0,0 +1,23 @@
+export const presentation =
+  "Sou apaixonado pela tecnologia e suas infinitas possibilidades. Tenho muito orgulho da minha história de vida, de como ela foi capaz de me proporcionar caminhos que hoje me trouxeram até esse momento profissional tão importante pra mim. Além disso, acredito que seja importante reafirmar que, como qualquer outra pessoa, estou em constante evolução, minha busca por aprendizagem é diária e constante. Para que essa evolução continue a acontecer, acredito que além de mim mesmo, as pessoas que me cercam também possuem poder de me trazer novos aprendizados e horizontes profissionais e pessoais. ";
+
+export const values = [
+  "Integridade ",
+  "Humildade",
+  "Empatia",
+  "Responsabilidade",
+  "Disciplina",
+  "Constância",
+];
+
+export const mission =
+  "Acredito que minha missão seja me tornar uma figura de força e inspiração para mim e para as pessoas que estão em minha vida. Essa missão só é possível quando seguida de princípios básicos como; escuta ativa, resiliência, empatia e atitude. ";
+
+export const strength = [
+  "Determinação",
+  "Comunicação",
+  "Proatividade",
+  "Liderança",
+  "Curiosidade",
+  "Criatividade",
+];
