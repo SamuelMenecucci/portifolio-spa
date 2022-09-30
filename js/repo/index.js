dee449f673e38222b3230a2d2dbdb97e14dd0a3b
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-09-30 16:23:44 -0300

    feat: repos from github api creation and styling

---
+++ js/repo/index.js
@@ -0,0 +1,43 @@
+export async function template() {
+  let data = await fetch("https://api.github.com/users/samuelmenecucci/repos");
+
+  let repositorios = await data.json();
+
+  return `
+${repositorios
+  .map((element) => {
+    return `
+  <div class="card repo">
+    <h2>${element.name}</h2>
+
+    <p>${element.description}</p>
+
+    <a href=${element.html_url} target="blank">Acessar Repositório</a>
+    
+  </div>  
+  `;
+  })
+  .join("")}
+  
+    `;
+}
+
+export async function renderRepo(main, style) {
+  main.innerHTML = "";
+
+  style.href = "./js/repo/styles.css";
+
+  let h1 = document.createElement("h1");
+
+  h1.innerText = "Meus Repositórios";
+
+  const cards = document.createElement("div");
+
+  cards.className = "cards";
+
+  cards.innerHTML = await template();
+
+  main.appendChild(cards);
+
+  cards.before(h1);
+}
